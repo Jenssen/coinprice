b62de09e4ed788d71c78960c64672781dd15c836
Robert Jenssen <jenssen.90@gmail.com>
2018-01-05 18:59:57 +0100

    Add navigation and footer components to landing page

---
+++ src/components/StandardFooter.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <!-- Hero footer: will stick at the bottom -->
+  <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li class="is-active"><a>Overview</a></li>
+          <li><a>Modifiers</a></li>
+          <li><a>Grid</a></li>
+          <li><a>Elements</a></li>
+          <li><a>Components</a></li>
+          <li><a>Layout</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'StandardFooter'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
