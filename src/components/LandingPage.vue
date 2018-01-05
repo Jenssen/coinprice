a9b154baa1a2103f1dd7757005a7817417fd8a3c
Robert Jenssen <jenssen.90@gmail.com>
2018-01-05 18:59:36 +0100

    Add landing page

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="hero is-success is-fullheight">
+    <!-- Hero head: will stick at the top -->
+    <navigation></navigation>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          Title
+        </h1>
+        <h2 class="subtitle">
+          Subtitle
+        </h2>
+      </div>
+    </div>
+
+    <standard-footer></standard-footer>
+
+  </section>
+</template>
+
+<script>
+import Navigation from './Navigation'
+import StandardFooter from './StandardFooter'
+
+export default {
+  components: {
+    'navigation': Navigation,
+    'standard-footer': StandardFooter
+  },
+  name: 'LandingPage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
