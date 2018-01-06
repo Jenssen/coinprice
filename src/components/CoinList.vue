6551010267a9be918f33f78abab0bed656068f28
Robert Jenssen <jenssen.90@gmail.com>
2018-01-06 01:57:32 +0100

    Fetch 100 more coins when pressing button.
    Also shows loading icon while loading

--- src/components/CoinList.vue
+++ src/components/CoinList.vue
@@ -1,24 +1,31 @@
 <template>
-  <nav class="level">
-    <p class="level-item has-text-centered">
-      <table class="table is-hoverable">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(coin, index) in coinList">
-            <th>{{ coin.rank }}</th>
-            <td>{{ coin.name }}</td>
-            <td>${{ coin.price }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </p>
-  </nav>
+  <div class="coin-list">
+    <nav class="level is-marginless">
+      <p class="level-item has-text-centered">
+        <table class="table is-hoverable">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(coin, index) in coinList">
+              <th>{{ coin.rank }}</th>
+              <td>{{ coin.name }}</td>
+              <td>${{ coin.price }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </p>
+    </nav>
+    <nav class="level">
+      <p class="level-item">
+        <button class="button" @click="loadMore" :class="{ 'is-loading': loading, 'is-danger': error }">Load more</button>
+      </p>
+    </nav>
+  </div>
 
 
 </template>
@@ -30,20 +37,32 @@ export default {
   name: 'CoinList',
   data: function () {
     return {
-      coinList: ''
+      start: 0,
+      limit: 100,
+      coinList: '',
+      loading: false,
+      error: false
     }
   },
   created: function () {
-    this.fetchData()
+    this.loading = true
+    this.fetchData(this.start, 100)
   },
   methods: {
-    fetchData: function () {
-      this.$http.get('https://api.coinmarketcap.com/v1/ticker/?limit=30')
+    fetchData: function (start, limit) {
+      this.$http.get('https://api.coinmarketcap.com/v1/ticker/?start=' +
+      start + '&limit=' + limit)
         .then(response => {
           this.coinList = ProcessResponse.process(response)
+          this.loading = false
         }, response => {
-          console.log('error')
+          this.error = true
         })
+    },
+    loadMore: function () {
+      this.loading = true
+      this.start += 100
+      this.fetchData(this.start, this.limit)
     }
   }
 }
@@ -51,5 +70,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.table {
+  margin: 0px;
+}
+.button {
+  margin: 24px;
+}
 </style>
